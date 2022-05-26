f2eab3ac254a4104b8b482b1f51ca7c4a4ae33cb
Martin Adams <martin@consciousapps.com>
2022-05-25 20:51:04 -0700

    [init] 🚀

--- chrome/hooks/auth/email/signin/index.js
+++ chrome/hooks/auth/email/signin/index.js
@@ -1,6 +1,5 @@
 // Packages
 import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 // Contexts
 import { useAuthCtx } from '-/auth'
 import { useLoadingCtx } from '-/loading'
@@ -8,8 +7,6 @@ import { useLoadingCtx } from '-/loading'
 import magic from '#/auth/magic'
 
 const HooksAuthEmailSignin = ({ email, submit }) => {
-	const router = useRouter()
-
 	const { setAuthStage } = useAuthCtx()
 
 	const { setLoading } = useLoadingCtx()
@@ -19,16 +16,9 @@ const HooksAuthEmailSignin = ({ email, submit }) => {
 			;(async () => {
 				setLoading(true)
 
-				const token = await magic.auth.loginWithMagicLink({
-					email,
-					redirectURI: new URL('/auth', window.location.origin).href
-				})
+				const token = await magic.auth.loginWithEmailOTP({ email })
 
-				if (token) {
-					setAuthStage(null)
-
-					router.push('/close')
-				}
+				if (token) setAuthStage(null)
 			})()
 		}
 	}, [submit])
