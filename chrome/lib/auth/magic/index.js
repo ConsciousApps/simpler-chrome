32409595bc879504b35fbc94e764a99f3719516e
Martin Adams <martin@consciousapps.com>
2022-05-25 21:01:59 -0700

    [init] 🚀

--- chrome/lib/auth/magic/index.js
+++ chrome/lib/auth/magic/index.js
@@ -4,7 +4,8 @@ import { OAuthExtension } from '@magic-ext/oauth'
 const LibAuthMagic = key =>
 	typeof window != 'undefined' &&
 	new Magic(key, {
-		extensions: [new OAuthExtension()]
+		extensions: [new OAuthExtension()],
+		testMode: process.env.NEXT_PUBLIC_ENV !== 'prd'
 	})
 
 export default LibAuthMagic(process.env.NEXT_PUBLIC_MAGIC)
