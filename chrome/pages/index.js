702e506704955e1c3a2e4984dffe3f90a993cc37
Martin Adams <martin@consciousapps.com>
2022-05-26 10:25:19 -0700

    [feat] Add to-do

--- chrome/pages/index.js
+++ chrome/pages/index.js
@@ -5,6 +5,7 @@ import useIsSignedIn from '=/app/isSignedIn'
 import useSignout from '=/auth/signout'
 // Layouts
 import Auth from '^/auth'
+import QuickAdd from '^/todo/quick'
 
 const PagesHome = () => {
 	const { authUser } = useAuthCtx()
@@ -13,7 +14,13 @@ const PagesHome = () => {
 
 	useSignout()
 
-	return <div className='p-4'>{!authUser && <Auth />}</div>
+	return (
+		<div className='p-4'>
+			{!authUser && <Auth />}
+
+			{authUser && <QuickAdd />}
+		</div>
+	)
 }
 
 export default PagesHome
