46584abae326902a83873b7fbeaa7e0d6d5fa474
Martin Adams <martin@consciousapps.com>
2022-08-05 12:21:06 -0700

    v2.0.2

--- chrome/pages/index.js
+++ chrome/pages/index.js
@@ -1,9 +1,15 @@
+// Packages
+import { SignedIn, SignedOut } from '@clerk/nextjs'
 // Layouts
 import Auth from '^/auth'
 
 const PagesHome = () => (
 	<div className='w-80 mx-auto p-4'>
-		<Auth />
+		<SignedIn>Hello, world</SignedIn>
+
+		<SignedOut>
+			<Auth />
+		</SignedOut>
 	</div>
 )
 
