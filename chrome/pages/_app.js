5107819578ba54178e761a8d47412dafae2ae3e2
Martin Adams <martin@consciousapps.com>
2022-08-05 12:11:59 -0700

    v2.0.2

--- chrome/pages/_app.js
+++ chrome/pages/_app.js
@@ -1,10 +1,15 @@
 // Packages
 import React from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
 // Global CSS
 import '@/output.css'
 // Languages
 import 'lang'
 
-const PagesApp = ({ Component, pageProps }) => <Component {...pageProps} />
+const PagesApp = ({ Component, pageProps }) => (
+	<ClerkProvider {...pageProps}>
+		<Component {...pageProps} />
+	</ClerkProvider>
+)
 
 export default PagesApp
